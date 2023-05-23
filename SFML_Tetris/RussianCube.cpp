9556739e6fcba1386dc6bbf3ac3ed214a7486b17
qhangz <qhangz@163.com>
2023-05-23 15:26:06 +0800

    5.23

---
+++ SFML_Tetris/RussianCube.cpp
@@ -0,0 +1,11 @@
+#include"RussianCubeGameDetail.h"
+#include"RussianCubeGame.h"
+#include"Head.h"
+int main()
+{
+    Game tetris;
+    tetris.gameRun();
+    //getchar();
+    //system("pause");
+    return 0;
+}
